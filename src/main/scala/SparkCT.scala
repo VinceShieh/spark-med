a9e3a0358866d86281f2c6035740dc83e133a745
VinceShieh <vincent.xie@intel.com>
2017-02-19 21:37:29 +0800

    update
    Signed-off-by: VinceShieh <vincent.xie@intel.com>

--- src/main/scala/SparkCT.scala
+++ src/main/scala/SparkCT.scala
@@ -2,6 +2,7 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.internal.Logging
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.mllib.tree.RandomForest
 //import org.apache.spark.ml.feature.LabeledPoint
 import org.apache.spark.ml.tree.impl.{GradientBoostedTrees => NewGBT}
 import org.apache.spark.ml.tuning.CrossValidator
@@ -71,18 +72,35 @@ object SparkCT extends App {
     println("Test with validation Error = " + testErr)
 
 */
-    val model2 = CGradientBoostedTrees.train(trainingData, boostingStrategy)
+    val modelGBT = CGradientBoostedTrees.train(trainingData, boostingStrategy)
 
     val labelAndPreds2 = testData.map { point =>
-      val prediction = model2.predict(point.features)
+      val prediction = modelGBT.predict(point.features)
       (point.label, prediction)
     }
 
     val testErr2 = labelAndPreds2.filter(r => r._1 != r._2).count.toDouble / testData.count()
-    println("Test without validation Error = " + testErr2)
+    println("GBT Test without validation Error = " + testErr2)
     //println("Learned classification GBT model:\n" + model.toDebugString)
 
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = args(1).toInt // Use more in practice.
+    val featureSubsetStrategy = "auto" // Let the algorithm choose.
+    val impurity = "gini"
+    val maxDepth = 5
+    val maxBins = 32
 
+    val modelRF = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+
+    // Evaluate model on test instances and compute test error
+    val labelAndPreds = testData.map { point =>
+      val prediction = modelRF.predict(point.features)
+      (point.label, prediction)
+    }
+    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+    println("GBT Test without validation Error = " + testErr2 + "RF Test Error = " + testErr)
   }
 }
 
