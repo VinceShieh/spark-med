fdf52f761f7a0b701a93acc644bcb0ec0f93023d
Vincent Xie <vincent.xie@intel.com>
2017-02-19 21:19:26 +0800

    commit from server
    Signed-off-by: Vincent Xie <vincent.xie@intel.com>

---
+++ run.sh
@@ -0,0 +1,13 @@
+$SPARK_HOME/bin/spark-submit \
+    --class SparkCT \
+    --master spark://sr443:7077 \
+    --driver-memory 180g \
+    --total-executor-cores 144 \
+    --executor-memory 180g \
+    --conf spark.kryoserializer.buffer.max=2047m \
+    --conf spark.driver.maxResultSize=20g \
+    target/scala-2.11/spark-med_2.11-0.0.1.jar \
+    hdfs://sr443/data/ctdata/* \
+    100 \
+    0.0001 \
+    144
--- src/main/scala/SparkCT.scala
+++ src/main/scala/SparkCT.scala
@@ -6,13 +6,13 @@ import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.tree.impl.{GradientBoostedTrees => NewGBT}
 import org.apache.spark.ml.tuning.CrossValidator
 import org.apache.spark.mllib.linalg.Vectors
-
 import org.apache.spark.mllib.regression._
 import org.apache.spark.mllib.tree.CGradientBoostedTrees
 import org.apache.spark.mllib.tree.configuration.{BoostingStrategy, Strategy}
 import org.apache.spark.mllib.tree.configuration.Algo._
 import org.apache.spark.mllib.tree.impurity.Variance
 import org.apache.spark.mllib.tree.loss.LogLoss
+import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -21,7 +21,7 @@ object SparkCT extends App {
 
   override def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setAppName("SparkCT").setMaster("local[4]")
+    val conf = new SparkConf().setAppName("SparkCT")
     val sc = new SparkContext(conf)
 
     // Set numIterations large enough so that it stops early.
@@ -29,34 +29,37 @@ object SparkCT extends App {
     //   val trainRdd = sc.parallelize(OldGBTSuite.trainData, 2).map(_.asML)
 
     //val data: DataFrame = spark.read.format("libsvm").load("data/ctdata/*")
-//    val data = MLUtils.loadLibSVMFile(sc, "data/ctdata/*").repartition(144)
-
-    val rawData = sc.textFile("data/5line", 4).map(_.split("\\s")).map(x => {
-      if (x(0).toInt > 3)
+    val data = MLUtils.loadLibSVMFile(sc, args(0)).repartition(args(3).toInt)
+/*
+    val rawData = sc.textFile(args(0)).map(_.split("\\s")).map(x => {
+      if (x(0).toInt > 0)
         x(0) = "1"
       else
         x(0) = "-1"
 
-      (x(0).toInt, x.drop(1).map(_.toDouble))
-    }).repartition(4)
+      val v = x.drop(1).map(_.split(":")).map( x => (x(0).toInt - 1, x(1).toDouble)).sortBy(_._1)
+      (x(0).toInt, v)
+    }).repartition(args(3).toInt)
 
+    val length = rawData.map(_._2.last._1).max + 1
 
-    val data: RDD[LabeledPoint] = rawData.map{case(label, v) => LabeledPoint(label, Vectors.dense(v))}
-
+    val data: RDD[LabeledPoint] = rawData.map{case(label, v) => LabeledPoint(label, Vectors.sparse(length,
+ v.map(_._1), v.map(_._2)))}
+*/
     // Split the data into training and test sets (30% held out for testing).
     val splits = data.randomSplit(Array(0.7, 0.3))
     val (trainingData, testData) = (splits(0), splits(1))
 
     val boostingStrategy = BoostingStrategy.defaultParams("Classification")
     boostingStrategy.treeStrategy.algo = Classification
-    boostingStrategy.numIterations = 50 // Note: Use more iterations in practice.
+    boostingStrategy.numIterations = args(1).toInt // Note: Use more iterations in practice.
     boostingStrategy.treeStrategy.numClasses = 2
     boostingStrategy.treeStrategy.maxDepth = 5
     boostingStrategy.treeStrategy.impurity = Variance
     // Empty categoricalFeaturesInfo indicates all features are continuous.
     boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
-    boostingStrategy.validationTol = 0.001
-
+    boostingStrategy.validationTol = args(2).toDouble
+/*
     val model = CGradientBoostedTrees.trainWithValidation(trainingData, testData, boostingStrategy)
     // Evaluate model on test instances and compute test error
     val labelAndPreds = testData.map { point =>
@@ -67,7 +70,7 @@ object SparkCT extends App {
     val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
     println("Test with validation Error = " + testErr)
 
-/*
+*/
     val model2 = CGradientBoostedTrees.train(trainingData, boostingStrategy)
 
     val labelAndPreds2 = testData.map { point =>
@@ -78,7 +81,7 @@ object SparkCT extends App {
     val testErr2 = labelAndPreds2.filter(r => r._1 != r._2).count.toDouble / testData.count()
     println("Test without validation Error = " + testErr2)
     //println("Learned classification GBT model:\n" + model.toDebugString)
-*/
+
 
   }
 }
